fc84dcaf401aad010dc2a66a4336c5a1510a1215
Al Natour Mazen <115185120+Al-Natour-Mazen@users.noreply.github.com>
2024-02-16 18:37:42 +0100

    add some funny things

--- app/src/main/java/fr/univ_poitiers/dptinfo/pokestat/InfoPokemon.java
+++ app/src/main/java/fr/univ_poitiers/dptinfo/pokestat/InfoPokemon.java
@@ -5,13 +5,18 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.bumptech.glide.Glide;
 import com.google.gson.Gson;
 import com.google.gson.JsonArray;
+import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
 
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.Objects;
 
 import okhttp3.OkHttpClient;
@@ -23,7 +28,9 @@ public class InfoPokemon extends AppCompatActivity {
     public static final String APP_TAG = "INFOPOKEMON";
 
     private String pokemonName;
-    private TextView pokeName, pokeSize, pokeWeight, pokeType;
+    private TextView pokeName, pokeSize, pokeWeight, pokeType, pokeHp;
+
+    private ImageView pokeImage;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,10 +38,13 @@ public class InfoPokemon extends AppCompatActivity {
         setContentView(R.layout.activity_info_pokemon);
 
         pokemonName = getIntent().getStringExtra("inputpokemonname");
+        pokeImage = findViewById(R.id.pokemonImage);
         pokeName = findViewById(R.id.textViewPSN);
         pokeSize = findViewById(R.id.textViewPSV);
         pokeWeight = findViewById(R.id.textViewPWV);
         pokeType = findViewById(R.id.textViewPTV);
+        pokeHp = findViewById(R.id.textViewPHP);
+
 
         if (pokemonName != null && !pokemonName.equals("") && pokeName != null) {
             pokeName.setText(pokemonName);
@@ -49,7 +59,7 @@ public class InfoPokemon extends AppCompatActivity {
         @Override
         protected JsonObject doInBackground(String... params) {
             OkHttpClient client = new OkHttpClient();
-            String url = "https://pokebuildapi.fr/api/v1/pokemon/" + params[0];
+            String url = "https://tyradex.vercel.app/api/v1/pokemon/" + params[0];
             Request request = new Request.Builder().url(url).build();
             try (Response response = client.newCall(request).execute()) {
                 if (!response.isSuccessful()) {
@@ -66,20 +76,34 @@ public class InfoPokemon extends AppCompatActivity {
 
         @Override
         protected void onPostExecute(JsonObject jsonObject) {
-            if (jsonObject != null) {
+            if (jsonObject.has("stats") && !jsonObject.get("stats").isJsonNull()) {
                 JsonObject stats = jsonObject.get("stats").getAsJsonObject();
-                String hp = stats.get("HP").getAsString();
 
-                String special_attack = stats.get("special_attack").getAsString();
+                String hp = stats.get("hp").getAsString();
+
+                JsonArray types = jsonObject.get("types").getAsJsonArray();
+                ArrayList<String> typeNames = new ArrayList<>();
+
+                for (JsonElement typeElement : types) {
+                    JsonObject typeObject = typeElement.getAsJsonObject();
+                    String typeName = typeObject.get("name").getAsString();
+                    typeNames.add(typeName);
+                }
+
+                String weight = jsonObject.get("weight").getAsString();
+                String height = jsonObject.get("height").getAsString();
 
 
-                JsonArray apiTypes = jsonObject.get("apiTypes").getAsJsonArray();
-                String typeName = apiTypes.get(0).getAsJsonObject().get("name").getAsString();
+                String imageUrl = jsonObject.get("sprites").getAsJsonObject().get("regular").getAsString();
+                Glide.with(InfoPokemon.this).load(imageUrl).into(pokeImage);
 
-
-                pokeSize.setText("HP: " + hp  );
-                pokeWeight.setText("Special Attack: " + special_attack );
-                pokeType.setText(typeName);
+                pokeSize.setText(height);
+                pokeWeight.setText(weight);
+                pokeType.setText(typeNames.toString());
+                pokeHp.setText(hp);
+            } else {
+                String message = getString(R.string.pokemon_not_found);
+                Toast.makeText(InfoPokemon.this, message, Toast.LENGTH_SHORT).show();
             }
         }
     }
